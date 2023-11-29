405d5695643f5b1aac293940a91e5cdad451c7c7
ak220193 <mail2meak22frcrio@gmail.com>
2023-11-29 21:14:08 +0530

    footer changes

--- src/components/Footer/MainFooter.jsx
+++ src/components/Footer/MainFooter.jsx
@@ -4,29 +4,33 @@ import logo from '../../assets/5_1.png'
 const MainFooter = () => {
   return (
       <div className='md:grid md:grid-cols-3 gap-4 py-8 px-5 md:mx-10 flex flex-col text-white'>
-          <div className='flex flex-col text-xs gap-3'>
+          <div className='flex flex-col text-sm gap-3'>
              
-              <h3>run567.co is Asia’s one of the biggest gaming platforms with over 350 games and sports available.</h3>
+              <h3>Betzhub is Asia’s one of the biggest gaming platforms with over 350 games and sports available.</h3>
               <h3>Popular sports such as Cricket, Tennis, Football, Basketball, Volleyball and many more including Indian card games like Teen Patti and Andar Bahar are also available from various worldwide gaming providers.</h3>
               <h3>With unlimited markets and odds, run567.co gives players the best gaming experience in the market.</h3>
           </div>
           <div className='flex flex-col gap-4'>
-            <div className='flex justify-center'>
-              <img src={logo} alt="Logo" className='w-30' />
+            <div className='flex justify-center' style={{ marginTop: '-15rem' }}>
+              <img src={logo} alt="Logo" className='w-21'/>
             </div>
             <div className='text-center'>
 
-              <h2 className='text-sm font-bold'>100% Safe & Instant Payments</h2>
-              <h3 className='text-xs'>You can make payments and receive earnings instantly via your UPI ID - so you can be sure that your money is safe and secure. All UPI platforms are accepted.</h3>
+              <h2 className='text-sm font-medium'>100% Safe & Instant Payments</h2>
+              <h3 className='text-sm font-medium'>You can make payments and receive earnings instantly via your UPI ID - so you can be sure that your money is safe and secure. All UPI platforms are accepted.</h3>
             </div>
+            <div className='flex justify-center  text-center'>
+            <h3 className='text-xs text-white px-6'>Visit Betzhub for tools and resources to help you stay in control of the way you play.
+            </h3>
+        </div>
           </div>
-          <div className='flex flex-col gap-3 text-center text-xs'>
-              <h2 className='text-sm font-bold'>Accepted Modes Of Payments</h2>
-              <img src={PaymentIcons} alt="Payment" className='h-20' />
+          <div className='flex flex-col gap-3 text-center text-2xl m-6'>
+              <h2 className='text-xl font-bold'>Accepted Modes Of Payments</h2>
+              <img src={PaymentIcons} alt="Payment" className='h-25' />
               <div className='flex flex-col justify-center text-center gap-2 w-full'>
-                  <h2 className='font-bold text-sm'>Contact Us</h2>
-                  <h3 className='text-gray-500'>Whatsapp :918618521090</h3>
-                  <h3 className='text-gray-500'>Calling  :918037244677</h3>
+                  <h2 className='font-bold text-2xl'>Contact Us</h2>
+                  <h3 className='text-gray-500 text-2xl'>Whatsapp :918618521090</h3>
+                  <h3 className='text-gray-500 text-2xl'>Calling  :918037244677</h3>
 
               </div>
           </div>
--- src/components/Footer/MiniFooter.jsx
+++ src/components/Footer/MiniFooter.jsx
@@ -4,11 +4,9 @@ const MiniFooter = () => {
     return (
         <div className='md:grid md:grid-cols-3 md:gap-4 my-8 flex flex-col justify-center items-center'>
             <div className='flex justify-center items-center'>
-                <img src={gamingHead} alt="Head" className='h-14' />
+                <img src={gamingHead} alt="Head" className='h-30' />
             </div>
             <div className='flex justify-center  text-center'>
-                <h3 className='text-xs text-white px-6'>Visit run567.co for tools and resources to help you stay in control of the way you play.
-                </h3>
             </div>
             <div className='lg:pl-28'>
                 <button className='bg-primary text-white text-sm font-semibold hover:bg-red-500 px-7 py-1.5 rounded-md'>Learn more</button>
